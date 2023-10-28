8ef2158c4606c966a7adeb5e3f8b3d644f775d7a
David-Jacobis <david.jacobis@outlook.com>
2023-10-28 15:52:04 -0300

    Adding the Initial Ambience

---
+++ LojaDeRoupaAPI/LojaDeRoupaAPI/Models/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LojaDeRoupaAPI.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+            
+        }
+    }
+}
